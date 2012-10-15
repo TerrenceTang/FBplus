e772daa48b80541bf286305a15bf22c3b7e5b9a9
terrence.tang <terrence.tang810@gmail.com>
2012-10-15 18:36:49 +0800

    init projection

---
+++ FBplus.js
@@ -0,0 +1,180 @@
+function FBplus() {
+    var args = Array.prototype.slice.call(arguments),
+        callback = args.pop(),
+        modules = (args[0] && typeof args[0] === "string") ?
+        args : args[0],
+        i;
+    if (!(this instanceof FBplus)) {
+ 　　　　 return new FBplus(modules, callback);
+ 　 }
+
+  　if (!modules || modules === '*') {
+      　modules = [];
+  　  　for (i in FBplus.modules) {
+   　     　if (FBplus.modules.hasOwnProperty(i)) {
+    　　　　　modules.push(i);
+    　　　　}
+   　 　}
+  　}
+
+ 　 for (i = 0; i < modules.length; i++) {
+      　FBplus.modules[modules[i]](this);
+    }
+
+    var isJquery = (typeof $ === "function") ? true : false;
+    this.send = function(type, link, params, scallback, fcallback){
+        var _fcallback,
+            _scallback,
+            _params = "";
+
+        if (typeof params === "object" || typeof params === "undefined") {
+            this.QueryParamsProjection(params);
+            if(type === "get") {
+                for (var name in params) {
+                console.log(name);
+                    if (typeof params[name] === "string" || typeof params[name] === "number") {
+                        _params += (name + "=" + params[name] + "&");
+                    }
+                }
+                _params += ("access_token=" + this.config.TOKEN);
+            }
+            else{
+                params.access_token = this.config.TOKEN;
+                _params = params;
+            }
+            _scallback = (scallback) ? scallback : this.SCALLBACK;
+            _fcallback = (fcallback) ? fcallback : this.FCALLBACK;
+        }
+        else {
+            _scallback = (params)    ? params    : this.SCALLBACK;
+            _fcallback = (scallback) ? scallback : this.FCALLBACK;
+            _params += ("access_token=" + this.config.TOKEN);
+        }
+        if(isJquery) {
+            $.ajax({
+                url:link,
+                data:_params,
+                dataType:"json",
+                type:type
+            }).done(_scallback).fail(_fcallback);
+        }
+        else {
+            console.error("no ajax object, default is used JQuery");
+        }
+    };
+　　callback(this);
+};
+
+FBplus.prototype.QueryParamsProjection = function(params){
+    var project = ["limit","offset","until","since"],
+        i;
+    for(i in params) {
+        if (params.hasOwnProperty(i)) {
+            if (project.indexOf(i) === -1) {
+                console.error("Fail, " + i + "is not correct parameter in this query");
+            }
+        }
+    }
+};
+
+/*
+ * using in user not set fail handle callback
+ */
+FBplus.prototype.SCALLBACK = function(res) {
+    console.error("need the parameter of success callback function");
+    console.log(res);
+};
+
+/*
+ * using in user not set success handle callback
+ */
+FBplus.prototype.FCALLBACK = function(res) {
+    console.error("need the parameter of fail callback function");
+    console.log(res);
+};
+
+FBplus.modules = {};
+
+FBplus.modules.config = function(box){
+    box.config = box.config || {};
+    box.config.FB_SERVER  = "https://graph.facebook.com/";
+    box.config.OAUTH_LINK = "https://www.facebook.com/dialog/oauth?";
+    box.config.TOKEN_LINK = "https://graph.facebook.com/oauth/access_token?";
+};
+
+FBplus.modules.auth = function(box){
+    box.auth = box.auth || {};
+    box.auth.getToken = function(){
+       return box.config.TOKEN;
+    };
+
+    box.auth.reloadToken = function(scallback, fcallback){
+        var _link = box.config.TOKEN_LINK + "client_id=" + this.config.CLIENT_ID + "&redirect_uri=" + this.config.REDIRECT_URI + "&secret_id=" + this.config.SECRET_ID + "&code=" + this.config.CODE;
+        this.send("get", _link, scallback, fcallback);
+    };
+
+    box.auth.getLoginUrl = function(scope){
+        return box.config.OAUTH_LINK + "client_id=" + box.config.CLIENT_ID + "&redirect_uri=" + box.config.REDIRECT_URI + "&scope=" + scope;
+    };
+
+    box.auth.getLogoutUrl = function(uri){
+        return "https://www.facebook.com/logout.php?next=" + uri + "&access_token=" + box.config.TOKEN;
+    };
+
+    box.auth.getLongLiveToken = function(scallback, fcallback){
+        var _link = "https://graph.facebook.com/oauth/access_token?client_id=" + this.config.CLIENT_ID + "&client_secret=" + this.config.SECRET_ID + "&grant_type=fb_exchange_token&fb_exchange_token=" + this.config.TOKEN;
+        this.send("get", _link, scallback, fcallback);
+    };
+};
+
+FBplus.modules.friends = function(box){
+    box.friends = box.friends || box;
+    box.friends.getFriendList = function(params, scallback, fcallback){
+        var link = box.config.FB_SERVER + "me/friends/";
+        box.send("get", link, params);
+    };
+    box.friends.getFriendInfo = function(fid, params, scallback, fcallback){
+         var link = box.config.FB_SERVER + fid;
+        box.send("get",link, params, scallback, fcallback);
+    }
+};
+
+FBplus.modules.checkin = function(box){
+    box.checkin = box.checkin || {};
+    box.checkin.getFriendCheckin = function(fid, params, scallback, fcallback){
+        var link = box.config.FB_SERVER + fid + "/checkins/";
+        box.send("get", link, params, scallback, fcallback);
+    },
+    box.checkin.getMeCheckin = function(params, scallback, fcallback){
+        box.checkin.getFriendCheckin("me", params, scallback, fcallback);
+    };
+
+    box.checkin.insert = function(fid, params){
+       var _fid    = (typeof fid === "string") ? fid : "me",
+           _params = (typeof fid === "object") ? fid : params,
+           link = box.config.FB_SERVER + _fid + "/checkins/";
+       box.send("post", link, _params);
+    };
+};
+
+FBplus.modules.wall = function(box){
+    box.wall = box.wall || {};
+    box.wall.insert = function(fid, params){
+       var _fid    = (typeof fid === "string") ? fid : "me",
+           _params = (typeof fid === "object") ? fid : params,
+           link = box.config.FB_SERVER + _fid + "/feed/";
+       box.send("post", link, _params);
+    };
+
+    box.wall.home = function(params){
+        var link = box.config.FB_SERVER + "me/home/";
+        box.send("get", link, params);
+    };
+
+    box.wall.feed = function(fid, params){
+        var _fid    = (typeof fid === "string") ? fid : "me",
+           _params = (typeof fid === "object") ? fid : params,
+           link = box.config.FB_SERVER + _fid + "/feed/";
+        box.send("get", link, _params);
+    };
+};
--- README.md
+++ README.md
@@ -1,4 +1,139 @@
 FBplus
 ======
 
-This is Facebook Plugin using to through by URL handle facebook data
+FBplus is a light javascript library for handling facebook data.<br>
+FBplus is used url to insert or query data, so developer doesn't need to load heavy library.<br>
+FBplus is build on jquery, so user needs to load jquery library before using FBplus
+
+#Setting
+In first, developer needs to set you app information
+
+```
+    FBplus.prototype.config = {
+        TOKEN:'access token',
+        CLIENT_ID:"your app id ",
+        SECRET_ID:"your secret id",
+        CODE:"user's code",
+        REDIRECT_URI:"after login, where will be redirect to "
+    };
+
+```
+
+* TOKEN ：
+* CLIENT_ID:
+* SECRET_ID:
+* CODE:
+* REDIRECT_URI:
+
+#Start
+FBplus is implemented by sandbox methodology. So developer needs to choose which module does he needs.
+
+```
+    new FBplus(['checkin', 'friends', 'config', 'auth', 'wall'], function(box){
+        // dosomethings
+    });
+
+    or
+
+    new FBplus("*", function(box){
+        // dosomethings
+    });
+```
+
+#Auth
+The module of Auth is using to help developer to create login url or deal token
+
+```
+    new FBplus(['auth'], function(box){
+        // the scope is about permession which will developer access
+        // more information to see [facebook permession](http://developers.facebook.com/docs/authentication/permissions/)
+        var scope = "read_stream,publish_checkins,publish_stream,user_status,user_checks,user_birthday,friends_status";
+
+        // get login link
+        box.auth.getLoginUrl(scope);
+
+        // get logout link
+        box.auth.getLogoutUrl()
+
+        //get long live token, this is danger to use in client, because it needs the secret id.
+        //add this feature is because the defuault timeout in facebook is 2 hours.
+        //In develop step, developer needs always re-load access token, it is very annoying.
+        box.auth.getLongLiveToken(success_callback_function, fail_callback_function);
+
+    });
+```
+
+#Wall
+The wall is about some activity of user or friend.
+
+```
+    new FBplus(['wall'], function(box){
+        // to get the home wall which include activitys of user and friends
+        box.wall.home(success_callback_function, fail_callback_function);
+        // to get the feed wall which bace on friend's id
+        box.wall.feed("fid");
+        // insert message into wall
+        box.wall.insert({message:"content"}, success_callback_function, fail_callback_function);
+    });
+```
+
+#Friends
+The module of friends shows friend list and information
+
+```
+    new FBplus(['friends'], function(box){
+        // show all friend
+        box.friends.getFriendList(success_callback_function, fail_callback_function);
+        // show information of friend
+        box.friends.getFriendInfo("fid", success_callback_function, fail_callback_function);
+        or
+        box.friends.getFriendInfo("me", success_callback_function, fail_callback_function);
+    });
+```
+
+#Checkin
+The module of checkin shows the checkin from user or friend
+
+```
+    new FBplus(['checkin'], function(box){
+        // show checkin from friend
+        box.checkin.getFriendCheckin("fid", success_callback_function, fail_callback_function);
+
+        // show checkin from me
+        box.checkin.getMeCheckin();
+
+        // make checkin
+        box.checkin.insert(
+            {
+               coordinates:
+               {
+                  latitude:"25.07698687147",
+                  longitude:"121.23201566872"
+               },
+               message:"test",
+               place:"142800992448822"
+            }
+        );
+    });
+```
+
+#Query Parameter
+When querying data, there are several useful parameters that enable you to filter and page through connection data:
+
+* limit
+* offset
+* until
+* since
+
+[more information](https://developers.facebook.com/docs/reference/api/pagination/)
+
+```
+   new FBplus(['checkin'], function(box){
+       // get 3 numbers data
+       box.checkin.getMeCheckin({limit:3});
+       // get checkin which create until yesterday
+       box.checkin,getMeChekin({until:"yesterday"});
+       // get checkin which was created  since yesterday
+       box.checkin,getMeChekin({since:"yesterday"});
+    });
+```
